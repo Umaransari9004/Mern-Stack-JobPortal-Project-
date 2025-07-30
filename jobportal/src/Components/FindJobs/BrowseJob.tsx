2274a22f1731f467bf64a8e88e5970913cbaa17f
Ansari Umar <Umaransari90045@gmail.com>
2025-07-30 16:08:20 +0530

    make a responsive

--- jobportal/src/Components/FindJobs/BrowseJob.tsx
+++ jobportal/src/Components/FindJobs/BrowseJob.tsx
@@ -37,7 +37,7 @@ const Browse = () => {
       <h1 className='font-bold text-xl my-10'>
         Search Results ({filteredJobs.length})
       </h1>
-      <div className='grid grid-cols-3 gap-4'>
+      <div className='p-4 w-80 sm-mx:w-full flex flex-col gap-4'>
         {filteredJobs.map((job) => (
           <JobCard key={job._id} {...job} />
         ))}
